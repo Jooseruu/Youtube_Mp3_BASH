323f722f8d79990cddecdad5f1ed62edf5a7d608
recoby <arocarivera@cifpfbmoll.eu>
2023-02-05 12:58:45 +0100

    Arreglamos el fallo del link

--- youtubemp3.sh
+++ youtubemp3.sh
@@ -30,9 +30,11 @@ else
   echo "No se instaló el paquete smplayer."
 fi
 
-#Se descargará utilizando url del video en cuestión, esta parte de aquí "$1" se utiliza como un marcador de posición.
-#para cuando se ejecute se reemplace por la url del video. Evidentemente "--extract-audio" indica que se extraera el audio del video.
+#Para cuando se ejecute se reemplace por la url del video. Evidentemente "--extract-audio" indica que se extraera el audio del video.
 #"--audio-format"  se utilizará para decidir el formato en el que se codificará el audio en este caso mp3.
 
-youtube-dl --extract-audio --audio-format mp3 "$1"
+# Preguntar al usuario el link de YouTube que desea descargar
+read -p "Introduce el enlace de YouTube que deseas descargar: " link
 
+# Descargar el video de YouTube como mp3
+youtube-dl --extract-audio --audio-format mp3 $link
